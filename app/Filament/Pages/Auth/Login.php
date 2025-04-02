8485d9c304c7686bf0b820f1edebdeb7a938ec17
ajespa-siae <informatica@esparreguera.cat>
2025-04-02 08:05:51 +0200

    Update Login.php

--- app/Filament/Pages/Auth/Login.php
+++ app/Filament/Pages/Auth/Login.php
@@ -104,16 +104,6 @@ class Login extends BaseLogin
                         $employeeId = $ldapUser->getFirstAttribute('employeeid');
                         
                         if (!$employeeId) {
-                            // Si no existe employeeID, buscar en otros atributos
-                            $possibleAttributes = [
-                                'employeenumber',
-                                'employeetype',
-                                'uid',
-                                'description',
-                                'extensionattribute1',
-                                'extensionattribute2'
-                            ];
-                            
                             foreach ($possibleAttributes as $attr) {
                                 $value = $ldapUser->getFirstAttribute($attr);
                                 if ($value) {
